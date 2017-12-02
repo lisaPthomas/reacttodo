8bbba47885c836c23803edfae8f1c6e5f6497636
Lisa Thomas <lisathomas@Lisas-MacBook-Pro-2.local>
2017-12-01 18:00:00 -0800

    prep for Heroku

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require("express");
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+	if(req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect("http://" + req.hostname + req.url);
+	}
+});
 
 //add functinoality to express app
 //this .use is the folder name you want to show to the public
 app.use(express.static('public'));
 
-//takes two args port and function will get called once server is up
-app.listen(3000, function(){
-  console.log("Express server is up and running on port 3000");
+
+//takes two args PORT and function will get called once server is up
+app.listen(PORT, function(){
+  console.log("Express server is up and running on PORT" + PORT);
 });
